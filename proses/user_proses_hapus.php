a556838b46e0a2fc45dc63a5d7ba4e79026df767
usmanit <usmanbcl7@gmail.com>
2018-01-11 09:43:22 +0700

    first commit

---
+++ proses/user_proses_hapus.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+	if (isset($_SESSION['login'])){
+		include 'koneksi.php';
+		$id = isset($_GET['ID']) ? $_GET['ID'] : '';
+		if (!empty($id)){
+			mysqli_query($connect, "DELETE FROM USER WHERE id = '$id'");
+			header("location:../user.php");
+		}else{
+			echo  "ID kosong";
+		}
+	}else{
+		echo "Please <a href='../index.php'>login</a>first.";
+		
+	}
+?>
